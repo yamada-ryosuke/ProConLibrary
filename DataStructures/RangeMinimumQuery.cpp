0c31cfe6cf0c240fb50e21b131c23b4f6f894661
Miyamoto Kaei <kaei199919@gmail.com>
2019-05-21 20:34:37 +0900

    非再帰化

--- DataStructures/RangeMinimumQuery.cpp
+++ DataStructures/RangeMinimumQuery.cpp
@@ -18,14 +18,7 @@ private:
 		unsigned int length{1};
 		while (length < array_size)
 			length <<= 1;
-		container_.resize(2 * length, inf);
-	}
-	long long getHelper(const int index, const int node_l, const int node_r, const int query_l, const int query_r) const
-	{
-		if (query_r <= node_l || node_r <= query_l) return inf_;
-		if (query_l <= node_l && node_r <= query_r) return container_[index];
-		const int node_m{(node_l + node_r) >> 1};
-		return std::min(getHelper(2 * index, node_l, node_m, query_l, query_r), getHelper(2 * index + 1, node_m, node_r, query_l, query_r));
+		container_.resize(2 * length, inf_);
 	}
 
 public:
@@ -51,7 +44,29 @@ public:
 	// left,rightは0-indexed、[left, right)の半開区間
 	long long get(const int left, const int right) const
 	{
-		return getHelper(1, 0, container_.size() >> 1, left, right);
+		// ノードの番号、左端、右端
+		using i3 = std::array<int, 3>;
+		std::stack<i3> pre_added;
+		pre_added.push({1, 0, (int)container_.size() >> 1});
+
+		long long min{inf_};
+		while (!pre_added.empty())
+		{
+			i3 added{pre_added.top()};
+			pre_added.pop();
+			if (added[2] <= left || right <= added[1])
+				continue;
+			
+			if (left <= added[1] && added[2] <= right)
+				min = std::min(min, container_[added[0]]);
+			else
+			{
+				int mid{(added[1] + added[2]) >> 1};
+				pre_added.push({2 * added[0], added[1], mid});
+				pre_added.push({2 * added[0] + 1, mid, added[2]});
+			}
+		}
+		return min;
 	}
 };
 
@@ -62,6 +77,7 @@ public:
 
 
 // http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_A
+// この問題ではinf_を(1ll << 31) - 1に書き換える必要がある
 int main()
 {
 	int n, q;
