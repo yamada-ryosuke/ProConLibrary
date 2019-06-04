b3404e1d149bd8b6eef7cb160c19f710658d79f2
Miyamoto Kaei <kaei199919@gmail.com>
2019-06-05 06:40:27 +0900

    change to MN Matrix and add operator= and operator^

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -8,8 +8,8 @@
 // 行列型 //
 ///////////
 
-// 加算、減算、乗算をもつN*M型行列
-// N=Mで環をつくる
+// 加算、減算、乗算をもつM×N型行列
+// M=Nで環をつくる
 template<typename T = int64_t>
 class Matrix {
 private:
@@ -18,14 +18,16 @@ private:
 	std::vector<RowVector> container_;
 
 public:
-	const int N, M;
+	const int M, N;
 
 	Matrix(const int row_size, const int column_size)
-		: N(row_size), M(column_size), container_(row_size, RowVector(column_size)){}
+		: M(row_size), N(column_size), container_(row_size, RowVector(column_size)){}
+	// ムーブコンストラクタ
 	Matrix(Matrix<T>&& mat)
-		: N(mat.N), M(mat.M), container_(std::move(mat.container_)){}
+		: M(mat.M), N(mat.N), container_(std::move(mat.container_)){}
+	// コピーコンストラクタ
 	Matrix(Matrix<T>& mat)
-		: N(mat.N), M(mat.M), container_(mat.container_){}
+		: M(mat.M), N(mat.N), container_(mat.container_){}
 
 	// 要素アクセス
 	RowVector& operator[](const int r) { return container_[r]; }
@@ -38,38 +40,58 @@ public:
 	Matrix<T> operator+(const Matrix<T>& mat) const
 	{
 		Matrix<T> ret{*this};
-		for (int r_i{}; r_i < N; r_i++)
-			for (int c_i{}; c_i < M; c_i++)
+		for (int r_i{}; r_i < M; r_i++)
+			for (int c_i{}; c_i < N; c_i++)
 				ret[r_i][c_i] += mat[r_i][c_i];
 		return std::move(ret);
 	}
 	Matrix<T> operator-(const Matrix<T>& mat) const
 	{
 		Matrix<T> ret{*this};
-		for (int r_i{}; r_i < N; r_i++)
-			for (int c_i{}; c_i < M; c_i++)
+		for (int r_i{}; r_i < M; r_i++)
+			for (int c_i{}; c_i < N; c_i++)
 				ret[r_i][c_i] -= mat[r_i][c_i];
 		return std::move(ret);
 	}
 	Matrix<T> operator*(const Matrix<T>& mat) const
 	{
-		Matrix<T> ret(this->N, mat.M);
-		for (int r_i{}; r_i < this->N; r_i++)
-			for (int c_i{}; c_i < mat.M; c_i++)
-				for (int l_i{}; l_i < this->M; l_i++)
+		Matrix<T> ret(this->M, mat.N);
+		for (int r_i{}; r_i < this->M; r_i++)
+			for (int c_i{}; c_i < mat.N; c_i++)
+				for (int l_i{}; l_i < this->N; l_i++)
 					ret[r_i][c_i] += (*this)[r_i][l_i] * mat[l_i][c_i];
 		return std::move(ret);
 	}
 	std::vector<T> operator*(const std::vector<T>& vec) const
 	{
-		std::vector<T> ret(N);
-		for (int r_i{}; r_i < N; r_i++)
-			for (int c_i{}; c_i < M; c_i++)
+		std::vector<T> ret(M);
+		for (int r_i{}; r_i < M; r_i++)
+			for (int c_i{}; c_i < N; c_i++)
 				ret[r_i] += (*this)[r_i][c_i] * vec[c_i];
 		return std::move(ret);
 	}
 
+	// 累乗
+	Matrix<T> operator^(const int64_t index)
+	{
+		Matrix<T> ret(M, M), pow(*this);
+		for (int i{}; i < M; i++)
+			ret[i][i] = 1ll;
+		for (int64_t i{index}; i > 0; i >>= 1)
+		{
+			if (i & 1)
+				ret *= pow;
+			pow *= pow;
+		}
+		return std::move(ret);
+	}
+
 	// 代入演算子
+	Matrix<T>& operator=(const Matrix& mat)
+	{
+		this->container_ = mat.container_;
+		return *this;
+	}	
 	Matrix<T>& operator+=(const Matrix& mat)
 	{
 		*this = *this + mat;
