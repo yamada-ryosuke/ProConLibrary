962072566074a12b2479d584d871a6e53e570e9f
TanakaTaroh <kaei199919@gmail.com>
2019-07-28 18:27:24 +0900

    fixed bug

--- Utilities/binPower.cpp
+++ Utilities/binPower.cpp
@@ -8,7 +8,7 @@ template <typename Monoid>
 Monoid binPower(const Monoid& radix, const int64_t exponent, const Monoid& identity)
 {
 	Monoid ret(identity), pow(radix);
-	for (int64_t i{expoent}; i > 0; i >> 1, pow = pow * pow)
+	for (int64_t i{exponent}; i > 0; i >>= 1, pow = pow * pow)
 		if (i & 1)
 			ret *= pow;
 	return std::move(ret);
